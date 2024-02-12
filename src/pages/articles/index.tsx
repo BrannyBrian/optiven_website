9483844bb87ff8c75c1de17896defd7cd24051a5
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-12 11:52:35 +0300

    chore: render articles from CMS

---
+++ src/pages/articles/index.tsx
@@ -0,0 +1,94 @@
+import { format } from "date-fns";
+import { fetcher } from "../../../lib/api";
+import Link from "next/link";
+import Stairs from "@/components/stairs";
+import { ChevronRight } from "react-feather";
+import Image from "next/image";
+
+const index = ({ articles }: any) => {
+  return (
+    <Stairs>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 sm:max-w-sm sm:mx-auto md:max-w-full">
+          {articles.data.map((article: any) => (
+            <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+              <Image
+                src={`${article.attributes.mainArticleImage.data.attributes.formats.small.url}`}
+                height={400}
+                width={700}
+                className="object-cover w-full h-64"
+                alt={`Image for ${article.attributes.articleName}`}
+              />
+              <div className="p-5 border border-t-0">
+                <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+                  {
+                    article.attributes.article_categories.data[0].attributes
+                      .articleCategoryName
+                  }
+                  <span className="text-gray-800">
+                    {" "}
+                    —{" "}
+                    {format(
+                      new Date(article.attributes.publishedAt),
+                      "MMMM dd, yyyy"
+                    )}
+                  </span>
+                </p>
+                <Link
+                  href={`articles/${article.id}`}
+                  className="secondary-text mb-3 text-2xl font-bold transition-colors duration-200 hover:text-green-600"
+                >
+                  {article.attributes.articleTitle}
+                </Link>
+                <p className="mb-2 text-gray-700">
+                  {article.attributes.articleIntro}
+                </p>
+                <Link
+                  href={`articles/${article.id}`}
+                  className="text-sm mt-4 flex un w-20 hover:text-green-600"
+                >
+                  Read More
+                  <ChevronRight size={16} />
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Stairs>
+  );
+};
+
+type Article = {
+  id: number;
+  attributes: {
+    articleTitle: string;
+    articleIntro: string;
+    articleBody: string;
+    locale: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  };
+};
+
+export async function getStaticProps() {
+  try {
+    const articlesResponse = await fetcher<Article[]>("articles?populate=*");
+
+    return {
+      props: {
+        articles: articlesResponse,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    return {
+      props: {
+        articles: [],
+      },
+    };
+  }
+}
+
+export default index;
