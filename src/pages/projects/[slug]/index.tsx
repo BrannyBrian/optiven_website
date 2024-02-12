cd15ade0cbd5bed57774300291dc637c9bbb6af9
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-12 10:59:33 +0300

    feat: implent markdown rendering

--- src/pages/projects/[slug]/index.tsx
+++ src/pages/projects/[slug]/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Accordion } from "flowbite-react";
 import Stairs from "@/components/stairs";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-// Define the type for the params object
 type Params = {
   params: {
     slug: string;
@@ -13,39 +14,16 @@ const Project = ({ project }: any) => {
   return (
     <Stairs>
       <section className="bg-white dark:bg-gray-900">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <div className="py-8 px-4 mx-auto max-w-screen-lg text-center lg:py-16">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             {project.data.attributes.projectName}
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
             {project.data.attributes.projectSummary}
           </p>
-          <Accordion>
-            <Accordion.Panel>
-              <Accordion.Title>Eighth Acre Price</Accordion.Title>
-              <Accordion.Content>
-                <p className="text-7xl mb-2 font-extrabold">
-                  {project.data.attributes.eighthAcrePrice}
-                </p>
-              </Accordion.Content>
-            </Accordion.Panel>
-            <Accordion.Panel>
-              <Accordion.Title>Quarter Acre Price</Accordion.Title>
-              <Accordion.Content>
-                <p className="text-7xl mb-2 font-extrabold">
-                  {project.data.attributes.quarterAcrePrice}
-                </p>
-              </Accordion.Content>
-            </Accordion.Panel>
-            <Accordion.Panel>
-              <Accordion.Title>Half Acre Price</Accordion.Title>
-              <Accordion.Content>
-                <p className="text-7xl mb-2 font-extrabold">
-                  {project.data.attributes.halfAcrePrice}
-                </p>
-              </Accordion.Content>
-            </Accordion.Panel>
-          </Accordion>
+          <Markdown className="text-start">
+            {project.data.attributes.projectContent}
+          </Markdown>
         </div>
       </section>
     </Stairs>
