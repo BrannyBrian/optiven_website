6a5b7a1aecd865feb2a0fb689c05b9ab62242e9f
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-12 12:03:25 +0300

    feat: render blog article

---
+++ src/pages/articles/[slug]/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Stairs from "@/components/stairs";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+const index = ({ article }: any) => {
+  return (
+    <Stairs>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-lg text-center lg:py-16">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            {article.data.attributes.articleTitle}
+          </h1>
+          <Markdown
+            className="text-start"
+            remarkPlugins={[remarkGfm]}
+          >
+            {article.data.attributes.articleBody}
+          </Markdown>
+        </div>
+      </section>
+    </Stairs>
+  );
+};
+
+export async function getServerSideProps({ params }: Params) {
+  const { slug } = params;
+  try {
+    const response = await fetch(
+      `${process.env.STRAPI_URL}articles/${slug}?populate=*/`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching article details: ${response.statusText}`);
+    }
+
+    const articleDetails = await response.json();
+
+    console.log(articleDetails);
+
+    return {
+      props: {
+        article: articleDetails,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching article details:", error);
+    return {
+      props: {
+        article: null,
+      },
+    };
+  }
+}
+
+export default index;
