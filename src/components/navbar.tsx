35afdb71dd5ab3b4be1915e40513e1486c34b297
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-13 14:21:48 +0300

    chore: navbar finito

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,21 @@
 import { Fragment, useState } from "react";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
-import { AlignRight, ChevronDown, Info, Users, X } from "react-feather";
+import {
+  AlignRight,
+  Camera,
+  ChevronDown,
+  Download,
+  Film,
+  Gift,
+  Info,
+  Loader,
+  Plus,
+  Radio,
+  Rss,
+  Users,
+  Video,
+  X,
+} from "react-feather";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,25 +36,77 @@ const aboutMenuItems = [
     name: "Why Us",
     description: "Get to know why we're trusted by thousands of Kenyans",
     href: "/why-us",
-    icon: ChevronDown,
+    icon: Loader,
   },
   {
     name: "Awards and Milestones",
-    description: "Connect with third-party tools",
+    description: "Explore our recognition and achievements",
     href: "/awards-and-milestones",
-    icon: ChevronDown,
+    icon: Gift,
   },
   {
     name: "Our Partners",
-    description: "Build strategic funnels that will convert",
+    description: "Discover our trusted network of partners",
     href: "/partners",
-    icon: ChevronDown,
+    icon: Users,
   },
   {
     name: "Corporate and Social Responsibility",
-    description: "Build strategic funnels that will convert",
+    description: "Learn about our commitment to society",
     href: "/corporate-and-social-responsibility",
-    icon: ChevronDown,
+    icon: Radio,
+  },
+];
+
+
+const infoCenterMenuItems = [
+  {
+    name: "Projects Updates",
+    description: "Get a better understanding of what we're about",
+    href: "/projects-updates",
+    icon: Info,
+  },
+  {
+    name: "Customer Information",
+    description: "Meet our core leadership team",
+    href: "/customer-information",
+    icon: Users,
+  },
+  {
+    name: "FAQs",
+    description: "Get to know why we're trusted by thousands of Kenyans",
+    href: "/faqs",
+    icon: Plus,
+  },
+  {
+    name: "Blogs & Company News",
+    description: "Connect with third-party tools",
+    href: "/news-and-blogs",
+    icon: Rss,
+  },
+  {
+    name: "Photo Gallery",
+    description: "Build strategic funnels that will convert",
+    href: "/photo-gallery",
+    icon: Camera,
+  },
+  {
+    name: "Video Gallery",
+    description: "Build strategic funnels that will convert",
+    href: "/video-gallery",
+    icon: Video,
+  },
+  {
+    name: "Newsletters & Downloads",
+    description: "Build strategic funnels that will convert",
+    href: "/newsletters-and-downloads",
+    icon: Download,
+  },
+  {
+    name: "Optiven in the Media",
+    description: "Build strategic funnels that will convert",
+    href: "/optiven-in-the-media",
+    icon: Film,
   },
 ];
 
@@ -144,7 +211,8 @@ export default function Navbar() {
             <Popover.Button className="text-lg flex items-center gap-x-1 leading-6 text-gray-900">
               About
               <ChevronDown
-                className="h-5 w-5 flex-none text-gray-700"
+                size={20}
+                className="text-gray-700"
                 aria-hidden="true"
               />
             </Popover.Button>
@@ -193,6 +261,53 @@ export default function Navbar() {
           >
             Testimonials
           </Link>
+          <Popover className="relative">
+            <Popover.Button className="text-lg flex items-center gap-x-1 leading-6 text-gray-900">
+              Information Center
+              <ChevronDown
+                size={20}
+                className="text-gray-700"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {infoCenterMenuItems.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon
+                          className="h-6 w-6 text-gray-600 group-hover:text-green-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="flex-auto">
+                        <Link
+                          href={item.href}
+                          className="block text-gray-700 font-semibold"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </Link>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
           <Link
             href="/careers"
             className=" leading-6 text-gray-900 text-lg un hover:text-green-600"
@@ -218,8 +333,12 @@ export default function Navbar() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+            <Link
+              href="/"
+              className="-m-1.5 p-1.5"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Optiven Limited</span>
               <Image
                 src="/optiven-logo.png"
                 alt="Optiven Logo"
@@ -240,6 +359,7 @@ export default function Navbar() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 <Link
+                  onClick={() => setMobileMenuOpen(false)}
                   href="/"
                   className="-mx-3 font-semibold block rounded-lg px-3 py-2 text-base leading-7 text-gray-700 hover:bg-gray-50"
                 >
@@ -261,6 +381,7 @@ export default function Navbar() {
                       <Disclosure.Panel className="mt-2 space-y-2">
                         {[...aboutMenuItems].map((aboutMenuItem) => (
                           <Disclosure.Button
+                            onClick={() => setMobileMenuOpen(false)}
                             key={aboutMenuItem.name}
                             as="a"
                             href={aboutMenuItem.href}
@@ -274,12 +395,43 @@ export default function Navbar() {
                   )}
                 </Disclosure>
                 <Link
+                  onClick={() => setMobileMenuOpen(false)}
                   href="/testimonials"
                   className="-mx-3 font-semibold block rounded-lg px-3 py-2 text-base leading-7 text-gray-700 hover:bg-gray-50"
                 >
                   Testimonials
                 </Link>
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <div>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-700 hover:bg-gray-50">
+                        Information Center
+                        <ChevronDown
+                          className={classNames(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...infoCenterMenuItems].map((infoCenterItem) => (
+                          <Disclosure.Button
+                            onClick={() => setMobileMenuOpen(false)}
+                            key={infoCenterItem.name}
+                            as="a"
+                            href={infoCenterItem.href}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-gray-900 hover:bg-gray-50"
+                          >
+                            - {infoCenterItem.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </div>
+                  )}
+                </Disclosure>
                 <Link
+                  onClick={() => setMobileMenuOpen(false)}
                   href="/careers"
                   className="-mx-3 font-semibold block rounded-lg px-3 py-2 text-base leading-7 text-gray-700 hover:bg-gray-50"
                 >
@@ -288,6 +440,7 @@ export default function Navbar() {
               </div>
               <div className="py-6">
                 <Link
+                  onClick={() => setMobileMenuOpen(false)}
                   href="/contact"
                   className="btn flex items-center justify-center px-4 py-2 rounded-md bg-green-700 text-white hover:bg-green-500"
                 >
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,60 +24,6 @@ const founderGrotesk = FoundersGrotesk({
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <main className={founderGrotesk.className}>
-      {/* <Navbar fluid rounded className="fixed top-0 w-full z-10 h-16">
-        <Navbar.Brand href="/">
-          <Image
-            src="/optiven-logo.png"
-            alt="Optiven Logo"
-            height={100}
-            width={160}
-          />
-        </Navbar.Brand>
-        <Navbar.Collapse>
-          <Navbar.Link href="/">Home</Navbar.Link>
-          <div>
-            <Dropdown
-              arrowIcon={true}
-              inline
-              label={<Navbar.Link href="#">About</Navbar.Link>}
-            >
-              <Dropdown.Item href="/about">Who We Are</Dropdown.Item>
-              <Dropdown.Item href="team">Our Team</Dropdown.Item>
-              <Dropdown.Item>Why Us</Dropdown.Item>
-              <Dropdown.Item>Awards & Milestones</Dropdown.Item>
-              <Dropdown.Item>Our Partners</Dropdown.Item>
-              <Dropdown.Item>Corporate Social Responsibility</Dropdown.Item>
-            </Dropdown>
-            <Navbar.Toggle />
-          </div>
-          <div>
-            <Dropdown
-              arrowIcon={true}
-              inline
-              label={<Navbar.Link href="#">Information Center</Navbar.Link>}
-            >
-              <Dropdown.Item href="/projects-updates">
-                Projects Updates
-              </Dropdown.Item>
-              <Dropdown.Item href="/customer-information">
-                Customer Information
-              </Dropdown.Item>
-              <Dropdown.Item href="/faqs">FAQs</Dropdown.Item>
-              <Dropdown.Item>News & Blogs</Dropdown.Item>
-              <Dropdown.Item href="/photo-gallery">Photo Gallery</Dropdown.Item>
-              <Dropdown.Item>Video Gallery</Dropdown.Item>
-              <Dropdown.Item>Newsletters & Downloads</Dropdown.Item>
-              <Dropdown.Item>Optiven in the Media</Dropdown.Item>
-            </Dropdown>
-            <Navbar.Toggle />
-          </div>
-          <Navbar.Link href="/testimonials">Testimonials</Navbar.Link>
-          <Navbar.Link href="/careers">Careers</Navbar.Link>
-        </Navbar.Collapse>
-        <Button href="/contact" className="bg-green-700">
-          Contact
-        </Button>
-      </Navbar> */}
       <Navbar />
       <AnimatePresence mode="wait">
         <Component key={router.route} {...pageProps} />
