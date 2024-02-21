342391f46248ec0514f225c3bcae416a6e5cf22b
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-21 10:19:38 +0300

    chore: add head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { AnimatePresence } from "framer-motion";
 import FoundersGrotesk from "@next/font/local";
 import "@/styles/globals.css";
@@ -24,6 +25,12 @@ const founderGrotesk = FoundersGrotesk({
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <main className={founderGrotesk.className}>
+      <Head>
+        <title>Optiven Group - Inspiring Possibilities</title>
+        <meta name="description" content="Inspiring Possibilities" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <AnimatePresence mode="wait">
         <div className="mt-32">
