df3a0c325803510620f5be53674321ef4bd52275
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-02-26 14:00:57 +0300

    fix: align text to the left in careers page

--- src/pages/careers/index.tsx
+++ src/pages/careers/index.tsx
@@ -39,9 +39,9 @@ export default function Careers({ careers, locations }: any) {
       </Head>
       <Stairs>
         <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8">
-          <div className="mx-auto sm:text-center lg:max-w-2xl">
-            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <div className="mx-auto lg:max-w-2xl">
+            <div className="max-w-xl mb-10 md:mx-auto lg:max-w-2xl md:mb-12">
+              <h2 className="mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
                 We're looking for YOU!
               </h2>
               <p className="text-base text-gray-700 md:text-lg">
@@ -56,7 +56,7 @@ export default function Careers({ careers, locations }: any) {
                 alt=""
               />
             </div>
-            <p className="max-w-xl mb-4 text-base text-gray-700 sm:mx-auto">
+            <p className="mb-4 text-base text-gray-700 sm:mx-auto">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud ullamco laboris aliquip ex ea.
@@ -90,9 +90,12 @@ export default function Careers({ careers, locations }: any) {
               <Card className="py-4" key={index}>
                 <div className="flex space-x-2">
                   <Briefcase />
-                  <h5 className="text-2xl font-bold">
+                  <Link
+                    href={`careers/${job.id}`}
+                    className="text-2xl font-bold"
+                  >
                     {job.attributes.jobTitle}
-                  </h5>
+                  </Link>
                 </div>
                 <div className="flex space-x-2">
                   <MapPin size={16} />
