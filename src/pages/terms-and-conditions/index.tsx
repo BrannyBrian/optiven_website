cea60e74fc2f419cc0f7965065908801a9907f0a
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-03-13 16:34:44 +0300

    chore: ts and qs

--- src/pages/terms-and-conditions/index.tsx
+++ src/pages/terms-and-conditions/index.tsx
@@ -1,12 +1,45 @@
 import React from "react";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 import Stairs from "@/components/stairs";
+import { fetcher } from "../../../lib/api";
 
-const index = () => {
+const index = ({ tsAndQs }: any) => {
+  const { termsAndConditionsContent } = tsAndQs.data[0].attributes;
+
   return (
     <Stairs>
-      <div>terms and conditions</div>
+      <section className="bg-white dark:bg-gray-900 flex justify-center items-center h-full">
+        <div className="max-w-xl w-full px-4 py-8">
+          <div className="format">
+            <BlocksRenderer content={termsAndConditionsContent} />
+          </div>
+        </div>
+      </section>
     </Stairs>
   );
 };
+
+export async function getStaticProps() {
+  try {
+    const tsAndQsResponse = await fetcher<any>(
+      "terms-and-conditions?populate=*"
+    );
+
+    // console.log(tsAndQsResponse.data);
+
+    return {
+      props: {
+        tsAndQs: tsAndQsResponse,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching cookie policy:", error);
+    return {
+      props: {
+        tsAndQs: [],
+      },
+    };
+  }
+}
 
 export default index;
