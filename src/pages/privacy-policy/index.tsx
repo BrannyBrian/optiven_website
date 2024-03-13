a95f2a423a0489bb44c34d153d4e6ae037d43bde
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-03-13 16:24:54 +0300

    chore: add privacy policy

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -1,12 +1,45 @@
 import React from "react";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 import Stairs from "@/components/stairs";
+import { fetcher } from "../../../lib/api";
 
-const index = () => {
+const index = ({ privacyPolicy }: any) => {
+  const { privacyPolicyDescription } = privacyPolicy.data[0].attributes;
+
   return (
     <Stairs>
-      <div>privacy policy</div>
+      <section className="bg-white dark:bg-gray-900 flex justify-center items-center h-full">
+        <div className="max-w-xl w-full px-4 py-8">
+          <div className="format">
+            <BlocksRenderer content={privacyPolicyDescription} />
+          </div>
+        </div>
+      </section>
     </Stairs>
   );
 };
+
+export async function getStaticProps() {
+  try {
+    const privacyPolicyResponse = await fetcher<any>(
+      "privacy-policies?populate=*"
+    );
+
+    // console.log(privacyPolicyResponse.data);
+
+    return {
+      props: {
+        privacyPolicy: privacyPolicyResponse,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching privacy policy:", error);
+    return {
+      props: {
+        privacyPolicy: [],
+      },
+    };
+  }
+}
 
 export default index;
