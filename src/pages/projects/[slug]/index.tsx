f5d851e40327870854bc0428ca45d40da07e261e
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-03-20 10:54:51 +0300

    chore: render carousel

--- src/pages/projects/[slug]/index.tsx
+++ src/pages/projects/[slug]/index.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from "react";
 import Stairs from "@/components/stairs";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 import Link from "next/link";
 import { ChevronRight } from "react-feather";
+import { Carousel } from "flowbite-react";
+import Image from "next/image";
 
 type Params = {
   params: {
@@ -10,16 +13,35 @@ type Params = {
 };
 
 const Index = ({ project }: any) => {
-  const { projectName } = project.data.attributes;
-  const { projectContent } = project.data.attributes;
-  const { subDivisionMapLink } = project.data.attributes;
-  const { onlineOfferLetterLink } = project.data.attributes;
-  const { waterApplicationFormLink } = project.data.attributes;
+  const {
+    projectName,
+    projectContent,
+    subDivisionMapLink,
+    onlineOfferLetterLink,
+    waterApplicationFormLink,
+  } = project.data.attributes;
+
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+  useEffect(() => {
+    const images = project.data.attributes.projectCarousel?.data.reduce(
+      (acc: string[], carousel: any) => {
+        const formats = carousel.attributes.formats;
+        const imageUrl =
+          formats.large?.url || formats.medium?.url || formats.small?.url;
+        if (imageUrl) acc.push(imageUrl);
+        return acc;
+      },
+      []
+    );
+
+    setImageUrls(images || []);
+  }, [project]);
 
   return (
     <Stairs>
       <section className="bg-white dark:bg-gray-900 flex justify-center items-center h-full">
-        <div className="max-w-xl w-full px-4 py-8">
+        <div className="max-w-2xl w-full px-4 py-8">
           <h1 className="text-4xl font-bold text-gray-700 lg:text-5xl dark:text-gray-400">
             {projectName}
           </h1>
@@ -61,6 +83,26 @@ const Index = ({ project }: any) => {
                 </Link>
               </div>
             </div>
+            {imageUrls.length > 0 && (
+              <div className="my-8">
+                <Carousel slideInterval={3000}>
+                  {imageUrls.map((imageUrl, index) => (
+                    <div
+                      key={index}
+                      className="relative"
+                      style={{ paddingBottom: "56.25%" }}
+                    >
+                      <Image
+                        src={imageUrl}
+                        layout="fill"
+                        objectFit="cover"
+                        alt={`project-carousel-banner-image-${index}`}
+                      />
+                    </div>
+                  ))}
+                </Carousel>
+              </div>
+            )}
           </div>
         </div>
       </section>
@@ -85,6 +127,8 @@ export async function getServerSideProps({ params }: Params) {
     }
 
     const projectDetails = await response.json();
+
+    console.log(projectDetails.data);
 
     return {
       props: {
