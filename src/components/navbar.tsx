af95bbf5c9aed5f7e26e55eebaff221478dcb772
James-Kabui <programmer@optiven.co.ke>
2024-04-25 19:06:37 +0300

    chore: update padding on information center popover

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -270,52 +270,53 @@ export default function Navbar() {
             Testimonials
           </Link>
           <Popover className="relative">
-            <Popover.Button className="text-lg flex items-center gap-x-1 leading-6 text-gray-900">
-              Information Center
-              <ChevronDown
-                size={20}
-                className="text-gray-700"
+  <Popover.Button className="text-lg flex items-center gap-x-1 leading-6 text-gray-900">
+    Information Center
+    <ChevronDown
+      size={20}
+      className="text-gray-700"
+      aria-hidden="true"
+    />
+  </Popover.Button>
+  <Transition
+    as={Fragment}
+    enter="transition ease-out duration-200"
+    enterFrom="opacity-0 translate-y-1"
+    enterTo="opacity-100 translate-y-0"
+    leave="transition ease-in duration-150"
+    leaveFrom="opacity-100 translate-y-0"
+    leaveTo="opacity-0 translate-y-1"
+  >
+    <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+      <div className="p-4 md:p-2">
+        {infoCenterMenuItems.map((item) => (
+          <div
+            key={item.name}
+            className="group relative flex items-center gap-x-6 rounded-lg p-4 md:p-2 text-sm leading-6 hover:bg-gray-50" 
+          >
+            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+              <item.icon
+                className="h-6 w-6 text-gray-600 group-hover:text-green-600"
                 aria-hidden="true"
               />
-            </Popover.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {infoCenterMenuItems.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon
-                          className="h-6 w-6 text-gray-600 group-hover:text-green-600"
-                          aria-hidden="true"
-                        />
-                      </div>
-                      <div className="flex-auto">
-                        <Link
-                          href={item.href}
-                          className="block text-gray-700 font-semibold"
-                        >
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </Link>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
+            </div>
+            <div className="flex-auto">
+              <Link
+                href={item.href}
+                className="block text-gray-700 font-semibold"
+              >
+                {item.name}
+                <span className="absolute inset-0" />
+              </Link>
+              <p className="mt-1 text-gray-600">{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Popover.Panel>
+  </Transition>
+</Popover>
+
           <Link
             href="/careers"
             className=" leading-6 text-gray-900 text-lg un hover:text-green-600"
