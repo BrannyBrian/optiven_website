d44fcf9e12b1354fae3f5697883f422151bfc343
James-Kabui <programmer@optiven.co.ke>
2024-04-29 13:09:41 +0300

    chore: Article cards are perfectly aligned

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -29,7 +29,7 @@ const index = ({ articles }: any) => {
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
         <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 sm:max-w-sm sm:mx-auto md:max-w-full">
           {articles.data.map((article: any) => (
-            <div className="overflow-hidden transition-shadow duration-300 bg-white">
+            <div className="overflow-hidden transition-shadow duration-300 bg-white h-full flex flex-col">
               <Image
                 src={
                   getBestAvailableImageUrl(
@@ -47,26 +47,26 @@ const index = ({ articles }: any) => {
                 className="object-cover w-full h-64 md:h-72 lg:h-80"
                 alt={`Image for ${article.attributes.articleName}`}
               />
-              <div className="p-5 border border-t-0">
-                <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-                  {article.attributes.articleCategory.data.map(
-                    (category: any) =>
-                      `${category.attributes.articleCategoryName} — `
-                  )}
-                  <span className="text-gray-800">
-                    {format(
-                      new Date(article.attributes.publishedAt),
-                      "MMMM dd, yyyy"
-                    )}
-                  </span>
-                </p>
+              <div className="flex flex-col flex-grow p-5 border border-t-0">
+              <p className="mb-3 text-xs font-semibold tracking-wide uppercase overflow-hidden">
+  {article.attributes.articleCategory.data.map(
+    (category: any, index: number, array: any[]) =>
+      `${category.attributes.articleCategoryName}${index < array.length - 1 ? ' — ' : ''}`
+  )}
+  <span className="text-gray-800">
+    {format(
+      new Date(article.attributes.publishedAt),
+      " - MMMM dd, yyyy"
+    )}
+  </span>
+</p>
                 <Link
                   href={`articles/${article.id}`}
-                  className="secondary-text mb-3 text-2xl font-bold transition-colors duration-200 hover:text-green-600"
+                  className="secondary-text mb-3 text-2xl font-bold transition-colors duration-200 hover:text-green-600 line-clamp-2"
                 >
                   {article.attributes.articleTitle}
                 </Link>
-                <p className="text-gray-700">
+                <p className="text-gray-700 flex-grow">
                   {article.attributes.articleIntro.length > 120
                     ? `${article.attributes.articleIntro.substring(0, 100)}...`
                     : article.attributes.articleIntro}
@@ -81,7 +81,7 @@ const index = ({ articles }: any) => {
                 </p>
                 <Link
                   href={`articles/${article.id}`}
-                  className="text-sm mt-4 flex un w-24 tracking-wide hover:text-green-600 font-bold"
+                  className="text-sm mt-4 flex items-end tracking-wide hover:text-green-600 font-bold"
                 >
                   Read More
                   <ChevronRight size={16} />
@@ -92,6 +92,7 @@ const index = ({ articles }: any) => {
         </div>
       </div>
     </Stairs>
+
   );
 };
 
