6301e3e3779e3cc92a9995539022cbb255f0b2b5
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-05-08 16:45:40 +0300

    chore: articles project nav, jumbotron

--- src/pages/articles/[slug]/index.tsx
+++ src/pages/articles/[slug]/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Stairs from "@/components/stairs";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
-import { ChevronsRight } from "react-feather";
-import { Popover } from "@headlessui/react";
 import Link from "next/link";
+import { fetcher } from "../../../../lib/api";
+import LocationList from "@/components/locationlist";
 
 type Params = {
   params: {
@@ -11,46 +11,109 @@ type Params = {
   };
 };
 
-const index = ({ article }: any) => {
+const index = ({ article, projects }: any) => {
   const { articleBody } = article.data.attributes;
+  // Extract projects with locations
+  const projectsWithLocationsArray = projects.data
+    .map((project: any) => project.attributes.projectLocation.data)
+    .filter((project: any) => project !== null);
+
+  // Deduplicate and sort locations
+  const uniqueLocations = Array.from(
+    new Set(
+      projectsWithLocationsArray.map(
+        (location: any) => location.attributes.projectLocation
+      )
+    )
+  ).sort();
   return (
     <Stairs>
-
-<div className="container mx-auto">
-  <ol className="flex justify-start space-x-2 rtl:space-x-reverse">
-    <Popover.Group className="hidden lg:flex lg:gap-x-4">
-      <li>
-        <Link href="/" className="block text-gray-700 font-semibold hover:text-green-500">
-          <span className="ml-1"> Home</span>
-        </Link>
-      </li>
-      <ChevronsRight
-      size={20}
-      className="text-gray-700"
-      aria-hidden="true"
-       />
-      <li>
-        <Link href="/articles" className="block text-gray-700 font-semibold hover:text-green-500">
-          Blogs & Company News 
-        </Link>
-      </li>
-      <ChevronsRight
-      size={20}
-      className="text-gray-700"
-      aria-hidden="true"
-       /> {article.data.attributes.articleTitle}
-    </Popover.Group>
-  </ol>
-</div>
+      <div className="bg-white dark:bg-gray-900 flex flex-col justify-center items-center h-full w-full">
+        {/* Breadcrumbs */}
+        <nav
+          className="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+          aria-label="Breadcrumb"
+        >
+          <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li className="inline-flex items-center">
+              <Link
+                href="/"
+                className="inline-flex items-center text-xs md:text-lg md:font-bold text-gray-700 hover:text-green-600 dark:text-gray-400 dark:hover:text-white"
+              >
+                <svg
+                  className="w-3 h-3 me-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                </svg>
+                Home
+              </Link>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <Link
+                  href="/articles"
+                  className="ms-1 text-xs md:text-lg md:font-bold text-gray-700 hover:text-green-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+                >
+                  Articles
+                </Link>
+              </div>
+            </li>
+            <li aria-current="page">
+              <div className="flex items-center">
+                <svg
+                  className="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <span className="ms-1 text-xs md:text-lg font-bold text-gray-500 md:ms-2 dark:text-gray-400">
+                  {article.data.attributes.articleTitle}
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+      </div>
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-lg lg:py-16">
-          <h1 className="text-4xl font-bold text-gray-700 lg:text-5xl sm:px-16 lg:px-48 dark:text-gray-400">
+          <h1 className="text-4xl text-start font-bold text-gray-700 lg:text-5xl sm:px-16 lg:px-48 dark:text-gray-400">
             {article.data.attributes.articleTitle}
           </h1>
-          <div className="my-4 text-lg text-gray-700 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-            <div className="format md:text-xl lg:text-2xl">
+          <div className="md:flex">
+            <div className="format md:text-xl md:w-1/2 lg:text-2xl lg:w-3/4">
               <BlocksRenderer content={articleBody} />
             </div>
+            <aside className="md:w-1/4 md:ml-2">
+              <LocationList locations={uniqueLocations} />
+            </aside>
           </div>
         </div>
       </section>
@@ -69,6 +132,7 @@ export async function getServerSideProps({ params }: Params) {
         },
       }
     );
+    const projectsResponse = await fetcher<any>("projects?populate=*");
 
     if (!response.ok) {
       throw new Error(`Error fetching article details: ${response.statusText}`);
@@ -80,6 +144,7 @@ export async function getServerSideProps({ params }: Params) {
     return {
       props: {
         article: articleDetails,
+        projects: projectsResponse,
       },
     };
   } catch (error) {
--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 import { fetcher } from "../../../lib/api";
 import Link from "next/link";
 import Stairs from "@/components/stairs";
-import { ChevronRight,  ChevronsRight } from "react-feather";
+import { ChevronRight, ChevronsRight } from "react-feather";
 import Image from "next/image";
 import { Popover } from "@headlessui/react";
 
@@ -27,32 +27,55 @@ const index = ({ articles }: any) => {
 
   return (
     <Stairs>
-
-<div className="container mx-auto">
-  <ol className="flex justify-start space-x-2 rtl:space-x-reverse">
-    <Popover.Group className="hidden lg:flex lg:gap-x-4">
-      <li>
-        <Link href="/" className="block text-gray-700 font-semibold hover:text-green-500">
-          <span className="ml-1"> Home</span>
-        </Link>
-      </li>
-      <ChevronsRight
-      size={20}
-      className="text-gray-700"
-      aria-hidden="true"
-       />
-      <li>
-        
-         Blogs & Company News
-        
-      </li>
-    </Popover.Group>
-  </ol>
-</div>
+      <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+        <div className="pt-8 px-4 mx-auto max-w-screen-xl text-center lg:pt-16 z-10 relative">
+          <Link
+            href="/projects"
+            className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-green-700 bg-green-100 rounded-full dark:bg-green-900 dark:text-green-300 hover:bg-green-200 dark:hover:bg-green-800"
+          >
+            <span className="text-xs bg-green-600 rounded-full text-white px-4 py-1.5 me-3">
+              Properties
+            </span>{" "}
+            <span className="text-sm font-medium">
+              Have a look at our various properties
+            </span>
+            <svg
+              className="w-2.5 h-2.5 ms-2 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+          </Link>
+          <div>
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              Discover Optiven's Latest Insights
+            </h1>
+            <p className="text-start mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
+              Delve into our collection of articles, company news, and blogs to
+              stay informed about the latest trends and developments in the
+              world of real estate investment. Optiven offers a wealth of
+              valuable insights to guide you on your investment journey.
+            </p>
+          </div>
+        </div>
+        <div className="bg-gradient-to-b from-green-50 to-transparent dark:from-green-900 w-full h-full absolute top-0 left-0 z-0" />
+      </section>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
         <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 sm:max-w-sm sm:mx-auto md:max-w-full">
           {articles.data.map((article: any) => (
-            <div className="overflow-hidden transition-shadow duration-300 bg-white">
+            <div
+              style={{ zIndex: 16 }}
+              className="overflow-hidden transition-shadow duration-300 bg-white"
+            >
               <Image
                 src={
                   getBestAvailableImageUrl(
@@ -96,7 +119,7 @@ const index = ({ articles }: any) => {
                   {article.attributes.articleIntro.length > 120 && (
                     <Link
                       href={`projects/${article.id}`}
-                      className="text-green-600 italic hover:underline"
+                      className="text-green-600 font-bold hover:underline"
                     >
                       read more
                     </Link>
