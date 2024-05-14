da7cd88b565b86fc532d84e9fa539ee27bf04ac5
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-05-14 16:20:04 +0300

    fix: porject to project

--- src/pages/customer-information/index.tsx
+++ src/pages/customer-information/index.tsx
@@ -43,7 +43,7 @@ const index: NextPage<PageProps> = ({ customerInfo }) => {
       <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
         <div className="pt-8 px-4 mx-auto max-w-screen-xl text-center lg:pt-16 z-10 relative">
           <Link
-            href="/porjects"
+            href="/projects"
             className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-green-700 bg-green-100 rounded-full dark:bg-green-900 dark:text-green-300 hover:bg-green-200 dark:hover:bg-green-800"
           >
             <span className="text-xs bg-green-600 rounded-full text-white px-4 py-1.5 me-3">
